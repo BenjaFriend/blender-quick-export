cd8da7ced625ea29c5180aa30bc3510de9af3538
Ben Hoffman <benjamin.hoffman.dev@gmail.com>
2018-06-01 17:17:10 -0400

    PArtly added command line args

--- importObjs.py
+++ importObjs.py
@@ -1,7 +1,20 @@
-## this script will import an obj file and then save it as a Blender file.
+## This purpose of this script is to import all .obj files from a directory,
+## and save them as .blend files. This is useful if you are, for example, downloading
+## assets off of Google poly, and want to edit the meshess or export it to a different
+## format.
 
-import os
-import bpy
+import os;
+import bpy;
+import sys;
+import argparse;
+
+parser = argparse.ArgumentParser();
+parser.parse_args();
+
+#print("\n\n++++++++++++++++\n");
+#    print(argv);
+#print("\n++++++++++++++++\n\n");
+
 
 # Select all and delete, so that we have gauranteee a clean scene
 def deleteAll():
@@ -12,18 +25,22 @@ def deleteAll():
 # Path to the object files
 full_path_to_directory = os.path.join(os.getcwd(), 'obj');
 
+# Get the list of files in this directory
 file_list = os.listdir(full_path_to_directory);
 
-# reduce the list to files ending in 'obj'
-# using 'list comprehensions'
-obj_list = [item for item in file_list if item[-3:] == 'obj']
+# reduce the list to files ending in 'obj' using 'list comprehensions'
+obj_list = [item for item in file_list if item[-3:] == 'obj'];
 
-
-
-# loop through the strings in obj_list.
+# loop through the strings in obj_list
 for item in obj_list:
     deleteAll();
     full_path_to_file = os.path.join(full_path_to_directory, item);
+    blend_file_name = os.path.splitext(full_path_to_file)[0];
     bpy.ops.import_scene.obj(filepath=full_path_to_file);
+    print ("=============== Import object ====================");
     # Save as new file
-    #bpy.ops.save_as_mainfile(full_path_to_file + '.blend');
+    blend_file_name = blend_file_name + '.blend';
+    print("-----------" + blend_file_name + '.blend');
+
+    bpy.ops.wm.save_as_mainfile(filepath=blend_file_name);
+    print ("+++++++++++ Saved file +++++++++++ " + blend_file_name + '.blend');
